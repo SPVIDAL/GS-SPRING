b0ac4df9a959506e35306f7793dcc53f4d95dc9b
Pedro H Vidal <101295809+SPVIDAL@users.noreply.github.com>
2023-05-30 20:37:52 -0300

    Add files via upload

---
+++ RadarFood/src/main/java/br/com/fiap/RadarFood/RadarFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.RadarFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadarFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadarFoodApplication.class, args);
+	}
+
+}
---
+++ RadarFood/src/test/java/br/com/fiap/RadarFood/RadarFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.RadarFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadarFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
